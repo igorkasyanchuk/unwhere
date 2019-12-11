aa4d7bd7d8df895ab1e299c402d99b1d24f387b1
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2019-12-11 14:09:59 -0800

    verified with scopes

--- test/dummy/app/models/user.rb
+++ test/dummy/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+
+  scope :admins, -> { where(first_name: 'Admin') }
+
 end
--- test/unwhere_test.rb
+++ test/unwhere_test.rb
@@ -5,5 +5,6 @@ class Unwhere::Test < ActiveSupport::TestCase
     assert_equal User.where(name: 'xx').to_sql, "SELECT \"users\".* FROM \"users\" WHERE \"users\".\"name\" = 'xx'"
     assert_equal User.where(name: 'xx').unwhere.to_sql, "SELECT \"users\".* FROM \"users\""
     assert_equal User.where(name: 'xx').unwhere.where(name: 'yy').to_sql, "SELECT \"users\".* FROM \"users\" WHERE \"users\".\"name\" = 'yy'"
+    assert_equal User.admins.unwhere.to_sql, "SELECT \"users\".* FROM \"users\""
   end
 end
